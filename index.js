21de67ee2effd199e0e8aff187f650fa0ccceac4
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2024-11-22 16:58:11 -0500

    Create function for playing one round

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Create variables to track computer and human scores
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // Get random integer between 0 and 2
     randInt = Math.floor(Math.random() * 3);
@@ -8,13 +12,13 @@ function getComputerChoice() {
     // Assign value to choice
     switch(randInt) {
         case 0:
-            choice = 'rock';
+            choice = 'ROCK';
             break;
         case 1:
-            choice = 'paper';
+            choice = 'PAPER';
             break;
         case 2:
-            choice = 'scissors';
+            choice = 'SCISSORS';
             break;
     };
 
@@ -28,3 +32,46 @@ function getHumanChoice() {
     // Return choice
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    // Convert human choice to uppercase for case-insensitive comparison
+    humanChoice = humanChoice.toUpperCase();
+
+    // IF computer and human choice the same log a tie
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else {
+        // ELSE check each possible human choice against each possible computer choice and log winner
+        if (humanChoice === 'ROCK') {
+            if (computerChoice === 'PAPER') {
+                console.log('Paper beats rock -- you lose.');
+                computerScore++;
+            } else {
+                console.log('Rock beats scissors -- you win!');
+                humanScore++;
+            };
+        } else if (humanChoice === 'PAPER') {
+            if (computerChoice === 'ROCK') {
+                console.log('Paper beats rock -- you win!');
+                humanScore++;
+            } else {
+                console.log('Scissors beat paper -- you lose.');
+                computerScore++;
+            };
+        } else {
+            if (computerChoice === 'ROCK') {
+                console.log('Rock beats scissors -- you lose.');
+                computerScore++;
+            } else {
+                console.log('Scissors beat paper -- you win!');
+                humanScore++;
+            };
+        };
+    };
+}
+
+// Declare choice variables
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
